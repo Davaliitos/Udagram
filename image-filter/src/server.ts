590709c755d0743b52507fd5333aecb50a6a9b6b
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-06-06 19:11:21 -0500

    Added filteredImage endpoint

--- image-filter/src/server.ts
+++ image-filter/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Request, Response } from '@types/express';
+import { filterImageFromURL, deleteLocalFiles, checkURL } from './util/utils';
 
 (async () => {
 
@@ -12,8 +14,31 @@ import bodyParser from 'body-parser';
     //Use the body parser middleware for post requests
     app.use(bodyParser.json());
 
+    app.get('/filteredImage', async( req : Request, res: Response) => {
+        const image_url : string = req.query.image_url as string;
+
+        if(!image_url){
+            return res.sendStatus(400);
+        }
+
+        if(!checkURL(image_url)){
+            return res.sendStatus(422);
+        }
+
+        const filteredImage = await filterImageFromURL(image_url);
+
+        if(!filteredImage){
+            return res.sendStatus(400);
+        }
+
+        res.sendFile(filteredImage, () => {
+            deleteLocalFiles([filteredImage])
+        });
+
+    })
+
     app.listen(port, () => {
         console.log( `server running http://localhost:${ port }` );
         console.log( `press CTRL+C to stop server` );
     })
-})
+})();
